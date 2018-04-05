4b79cb0d440d51749d54ff043ee299311ade5a10
Home_W7_01\carl <ctu@hyphensolutions.com>
2018-04-04 23:39:11 -0500

    change file1 and carlTest

--- app/scripts/carlTest.js
+++ app/scripts/carlTest.js
@@ -4,4 +4,25 @@ function test() {
 
     var platform = 'macbook';
     var newVar = 'new Var';
+
+
+    /*comment*/
+    $scope.imageTypeEnum = {
+        MODEL: 1,
+        SERIAL: 2,
+        PROTECT: 3
+        };
 }
+
+
+function(message) {
+    $timeout(function() {
+      // iOS permission issue
+      if (message === 'has no access to assets') {
+        $cordovaDialogs.alert('Cancelled taking a picture', null, 'Camera');
+      }
+      else {
+        $cordovaDialogs.alert(message, null, 'Camera');
+      }
+    }, 0);
+  });
--- app/scripts/file1.js
+++ app/scripts/file1.js
@@ -1,3 +1,28 @@
 var name22 = 'file1 name 2';
 var name33 = 'file1 name 3';
 var name44 = 'file1 name 4';
+
+function (message) {
+    $timeout(function() {
+      // iOS permission issue
+      if (message === 'has no access to assets') {
+        $cordovaDialogs.alert('Cancelled taking a picture', null, 'Camera');
+      }
+      else {
+        $cordovaDialogs.alert(message, null, 'Camera');
+      }
+
+      $scope.imageTypeEnum = {
+        MODEL: 1,
+        SERIAL: 2,
+        PROTECT: 3
+        };
+
+
+
+    }, 0);
+  });
+
+var name22 = 'file1 name 2';
+var name33 = 'file1 name 3';
+var name44 = 'file1 name 4';
