bf4f399965045e742094b4d2822651700480f7a7
Home_W7_01\carl <ctu@hyphensolutions.com>
2018-04-04 23:40:38 -0500

    major changes

--- app/scripts/carlTest.js
+++ app/scripts/carlTest.js
@@ -1,7 +1,20 @@
 function test() {
-    var test1 = '';
-    var this_is_my_test = '';
+    $timeout(function() {
 
-    var platform = 'macbook';
-    var newVar = 'new Var';
-}
+    
+    /*comment*/
+    $scope.imageTypeEnum = {
+        MODEL: 1,
+        SERIAL: 2,
+        PROTECT: 3
+        };
+    
+      // iOS permission issue
+      if (message === 'has no access to assets') {
+        $cordovaDialogs.alert('Cancelled taking a picture', null, 'Camera');
+      }
+      else {
+        $cordovaDialogs.alert(message, null, 'Camera');
+      }
+    }, 0);
+  });
--- app/scripts/file1.js
+++ app/scripts/file1.js
@@ -1,3 +1,15 @@
 var name22 = 'file1 name 2';
 var name33 = 'file1 name 3';
 var name44 = 'file1 name 4';
+
+function(message) {
+    $timeout(function() {
+      // iOS permission issue
+      if (message === 'has no access to assets') {
+        $cordovaDialogs.alert('Cancelled taking a picture', null, 'Camera');
+      }
+      else {
+        $cordovaDialogs.alert(message, null, 'Camera');
+      }
+    }, 0);
+  });
