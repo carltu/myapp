c3cd54cbcc7e3f83ed4b8a2563b1332aa501a72b
carltu <ctu@hyphensolutions.com>
2018-04-08 10:58:13 -0500

    touch file2 and add file3

--- app/scripts/file3.js
+++ app/scripts/file3.js
@@ -1,9 +1,6 @@
-var name5 = 'file1 name 5';
-var name6 = 'file1 name 6';
-var name7 = 'file1 name 7';
-var name8 = 'file1 name 8';
-
 function(message) {
+    var file3 = '';
+
     $timeout(function() {
       // iOS permission issue
       if (message === 'has no access to assets') {
@@ -14,6 +11,3 @@ function(message) {
       }
     }, 0);
   });
-
-
-
