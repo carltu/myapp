e6a35fd961dcae624f8d90562a1c1d88a09c42bb
carltu <ctu@hyphensolutions.com>
2019-01-22 12:22:10 -0600

    a lot of changes

--- app/scripts/demo.js
+++ app/scripts/demo.js
@@ -1,14 +1,24 @@
 // i should get a confilict here
 var test = 'this is a new merging test for file';
 var test3 = 'test2';
-var test = 'for my merging test origianl';
-var vivian = 'this is a new merging test ';
+
 var vivian2 = 'this is a new merging test for an old file';
 // this is a new change
 var kevin = 'change from kevin to carl';
 var newVar = 'robbie';
-var newVar2 = 'carl';
-var newVar3 = 'carl 2';
+if (1 ==1) {
+    var newVar2 = 'carl';
+    var newVar3 = 'carl 2';
+}
+
 var newVar4 = 'robbie 2';
 var kevin2 = 'kevin2';
-
+$timeout(function() {
+    // iOS permission issue
+    if (message === 'has no access to assets') {
+      $cordovaDialogs.alert('Cancelled taking a picture', null, 'Camera');
+    }
+    else {
+      $cordovaDialogs.alert(message, null, 'Camera');
+    }
+  }, 0);
