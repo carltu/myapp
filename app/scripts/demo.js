7cf82ce44537b6a8766d2211a88091666ca6db2e
carltu <ctu@hyphensolutions.com>
2019-01-22 12:19:32 -0600

    make some changes

--- app/scripts/demo.js
+++ app/scripts/demo.js
@@ -4,8 +4,8 @@ var test3 = 'test2';
 var test = 'for my merging test origianl';
 var vivian = 'this is a new merging test ';
 var vivian2 = 'this is a new merging test for an old file';
-
-var kevin = 'kevin';
+// this is a new change
+var kevin = 'change from kevin to carl';
 var newVar = 'robbie';
 var newVar2 = 'carl';
 var newVar3 = 'carl 2';
