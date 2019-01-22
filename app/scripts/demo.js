858dbf32a284a8d41f6358707e3beec019a6df1e
carltu <ctu@hyphensolutions.com>
2019-01-22 11:49:28 -0600

    add 2 variables

--- app/scripts/demo.js
+++ app/scripts/demo.js
@@ -5,8 +5,10 @@ var test = 'for my merging test origianl';
 var vivian = 'this is a new merging test ';
 var vivian2 = 'this is a new merging test for an old file';
 
+var kevin = 'kevin';
 var newVar = 'robbie';
 var newVar2 = 'carl';
 var newVar3 = 'carl 2';
 var newVar4 = 'robbie 2';
+var kevin2 = 'kevin2';
 
